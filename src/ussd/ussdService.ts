c42fee5b6f270f00332d9caaef8e097069b7bd2f
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-26 00:59:43 +0300

    USSD Services

---
+++ src/ussd/ussdService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const registerUser = () => {
+
+}
+
+const sendSingleSms = (phoneNumber: string, message:string) => {
+
+}
+
+const findUserByPhoneNumber = (mobileNUmber: string) => {
+    return false;
+}
+
+export {
+    findUserByPhoneNumber,
+    registerUser
+}
