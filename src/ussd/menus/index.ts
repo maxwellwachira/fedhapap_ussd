d569c9469b42e88cc1761f524e36fe19ba75bc1d
maxwellwachira <maxwellwachira67@gmail.com>
2022-08-26 22:53:07 +0300

    all USSD menus

---
+++ src/ussd/menus/index.ts
@@ -0,0 +1,11 @@
+import { goBack, goToMainMenu, languageMenu } from "./sharedMenus";
+import EnglishMenus from "./englishMenus";
+import KiswahiliMenus from "./kiswahiliMenus";
+
+export {
+    EnglishMenus,
+    goBack,
+    goToMainMenu,
+    KiswahiliMenus,
+    languageMenu,
+};
