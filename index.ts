2ce767af9292f9799192d96c06ed757fb676eed7
maxwellwachira <maxwellwachira67@gmail.com>
2022-09-23 12:34:16 +0300

    added health check endpoint

--- index.ts
+++ index.ts
@@ -12,6 +12,10 @@ const app : Application = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get('/health', (req: Request, res: Response) => {
+    return res.status(200).json({ping: "pong"});
+});
+
 app.post('/', Listener);
 
 app.listen(port, () => {
